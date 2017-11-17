36e0ab6258ea52bf1ecd92d1cb1031bc3c377020
635r <31050392+635r@users.noreply.github.com>
2017-11-17 15:25:59 -0800

    Initial commit

---
+++ BASIC PYTHON NOTES.py
@@ -0,0 +1,15 @@
+print("Hello World")
+print("Welcome To Class")
+
+# THIS IS A COMMENT
+# IT IS IGNORED BY CODE
+
+'''THIS IS TEXT. IT GETS IGNORED SINCE IT IS INBETWEEN A SET OF THREE QUOTATION MARKS'''
+
+a = "Howdy-do"
+print(a)
+
+b = 5
+c = 7
+print(b+c)
+print(-1- (-5))
